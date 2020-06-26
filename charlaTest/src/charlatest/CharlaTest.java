7b3b7e3f16e345d0815ed9c1c51ff3c516f44a4c
oscarm081297 <50888671+oscarm081297@users.noreply.github.com>
2020-06-25 19:05:42 -0600

    cambio en CharlaTest.java main

--- charlaTest/src/charlatest/CharlaTest.java
+++ charlaTest/src/charlatest/CharlaTest.java
@@ -15,7 +15,11 @@ public class CharlaTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String s1 = "Hola";
+        
+        System.out.println(s1);
+        
     }
     
 }
